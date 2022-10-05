a2820145dfcdd82ca1e534658b8bd38aa82c4c4d
bbw3luv <85767712+bbw3luv@users.noreply.github.com>
2022-10-05 13:07:30 -0500

    Update variable.tf

--- myApp/variable.tf
+++ myApp/variable.tf
@@ -25,4 +25,30 @@ variable "sg_ingress_rules" {
         },
     ]
 }
+variable "sg_egress_rules" {
+    type = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_block  = string
+      description = string
+    }))
+    default     = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_block  = "1.2.3.4/32"
+          description = "test"
+        },
+        {
+          from_port   = 23
+          to_port     = 23
+          protocol    = "tcp"
+          cidr_block  = "1.2.3.4/32"
+          description = "test"
+        },
+    ]
+}
+
 
